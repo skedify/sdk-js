eb4f0552b2a797f03c425e9a158c800a51af1de4
Robin Malfait <malfait.robin@gmail.com>
2017-12-31 13:44:25 +0100

    refactor: make the assert function a bit more readable

--- src/decorators/withShorthands/assert.js
+++ src/decorators/withShorthands/assert.js
@@ -6,21 +6,29 @@ import {
 import joinAsSpeech, { AND } from '../../util/joinAsSpeech'
 
 export default function assert(call, given, required = Object.keys(given)) {
-  const missing = required
-    .filter(key => given[key] === undefined)
-    .map(key => `\`${key}\``)
+  const actualGivenKeys = Object.keys(given).filter(
+    key => given[key] !== undefined
+  )
 
-  const actualGiven = Object.keys(given)
-    .filter(key => given[key] !== undefined)
-    .reduce((res, key) => Object.assign(res, { [key]: given[key] }), {})
+  const actualGiven = actualGivenKeys.reduce(
+    (res, key) => Object.assign(res, { [key]: given[key] }),
+    {}
+  )
+
+  const missing = required
+    .filter(key => !actualGivenKeys.includes(key))
+    .map(key => `\`${key}\``)
 
   if (missing.length > 0) {
     throw createResourceError(
       `You tried to call .${call}(${JSON.stringify(
         actualGiven
-      )}) but ${joinAsSpeech(AND, missing)} ${
-        missing.length === 1 ? 'is' : 'are'
-      }${Object.keys(actualGiven).length > 0 ? ' also' : ''} required.`,
+      )}) but ${joinAsSpeech(AND, missing)} ${[
+        missing.length === 1 ? 'is' : 'are',
+        actualGivenKeys.length > 0 && 'also',
+      ]
+        .filter(Boolean)
+        .join(' ')} required.`,
       ERROR_RESOURCE,
       ERROR_RESOURCE_MISSING_ARGUMENT
     )
