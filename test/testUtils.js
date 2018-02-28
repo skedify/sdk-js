6a6baa4da3cfc0f87c51656c7a24a8e501dec141
Robin Malfait <malfait.robin@gmail.com>
2018-02-28 15:26:09 +0100

    fix: only mock access_tokens and proxy call when needed

--- test/testUtils.js
+++ test/testUtils.js
@@ -4,41 +4,39 @@ const WAIT_TIME = 0
 
 export function mockResponse(data, meta, warnings, status = 200) {
   // Mock the authentication request
+  moxios.stubRequest(/access_tokens/, {
+    status: 200,
+    response: {
+      access_token: 'fake_example_access_token',
+      token_type: 'Bearer',
+      expires_in: 5400,
+    },
+  })
+
+  // Mock the proxy call
+  moxios.stubRequest(/\/integrations\/proxy/, {
+    status: 200,
+    response: {
+      data: {
+        // We need to use a getter to delay the execution of this function
+        // We need access to the mostRecent request, but if there is no most recent
+        // Request we can't access its url. Therefor once we are sure the /integrations/proxy
+        // Is executed, we can safely run the mostRecent() function.
+        get url() {
+          return moxios.requests
+            .mostRecent()
+            .url.replace('/integrations/proxy', '')
+        },
+      },
+    },
+  })
+
+  // Mock the actual request
   moxios.wait(() => {
     moxios.requests.mostRecent().respondWith({
-      status: 200,
-      response: {
-        access_token: 'fake_example_access_token',
-        token_type: 'Bearer',
-        expires_in: 5400,
-      },
+      status,
+      response: { data, meta, warnings },
     })
-
-    // We assume here that the url used to request access_tokens is the base url
-    // minus the `/access_tokens`. This is easier when we test configuration changes.
-    const proxy_url = moxios.requests
-      .mostRecent()
-      .config.url.replace('/access_tokens', '')
-
-    // Mock the proxy call
-    moxios.wait(() => {
-      moxios.requests.mostRecent().respondWith({
-        status: 200,
-        response: {
-          data: {
-            url: proxy_url,
-          },
-        },
-      })
-
-      // Mock the actual request
-      moxios.wait(() => {
-        moxios.requests.mostRecent().respondWith({
-          status,
-          response: { data, meta, warnings },
-        })
-      }, WAIT_TIME)
-    }, WAIT_TIME)
   }, WAIT_TIME)
 }
 
