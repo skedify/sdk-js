9cd898d3bb11aef2585b486cf4aea745cfd2d8b0
Robin Malfait <malfait.robin@gmail.com>
2018-07-27 16:45:42 +0200

    feat: add availabilityHours and availabilityHoursExceptions

--- src/resources/contacts.js
+++ src/resources/contacts.js
@@ -1,13 +1,28 @@
 import createResourceDescription from './util/createResourceDescription'
 
-export const contacts = createResourceDescription('contacts', {
-  includes: [
-    'user',
-    'offices',
-    'offices.subject_settings',
-    'subjects',
-    'subjects.subject_category',
-    'appointments',
-  ],
-  filters: ['offices', 'schedulable'],
-})
+export const contacts = createResourceDescription(
+  'contacts',
+  {
+    includes: [
+      'user',
+      'offices',
+      'offices.subject_settings',
+      'subjects',
+      'subjects.subject_category',
+      'appointments',
+    ],
+    filters: ['offices', 'schedulable'],
+  },
+  {
+    offices: createResourceDescription(
+      'offices',
+      {},
+      {
+        availabilityHours: createResourceDescription('availability_hours'),
+        availabilityHoursExceptions: createResourceDescription(
+          'availability_hours_exceptions'
+        ),
+      }
+    ),
+  }
+)
