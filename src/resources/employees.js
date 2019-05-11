e185cd6e681e5ae33d453354d3225b9e3089003b
Demian Dekoninck <demian@live.be>
2019-05-11 18:35:24 +0200

    feat: add support for suppressing of activation emails

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,6 +1,18 @@
 import createResourceDescription from './util/createResourceDescription'
+import { HTTP_VERB_POST } from '../constants'
 
 export const employees = createResourceDescription('employees', {
   includes: ['user', 'enterprise', 'roles'],
   filters: ['email', 'external_id'],
+  headers: {
+    [HTTP_VERB_POST]: {
+      suppress_activation_email: value => {
+        if (value !== undefined) {
+          return { 'X-suppress-activation-email': true }
+        }
+
+        return undefined
+      },
+    },
+  },
 })
