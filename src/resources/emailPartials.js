14421c307c81448f3975e64f198cc11087adcfd8
Demian Dekoninck <demian@live.be>
2020-08-19 19:36:32 +0200

    feat: add resources for email templates and email partials

---
+++ src/resources/emailPartials.js
@@ -0,0 +1,5 @@
+import createResourceDescription from './util/createResourceDescription'
+
+export const emailPartials = createResourceDescription('email_partials', {
+  enable_pagination: false,
+})
