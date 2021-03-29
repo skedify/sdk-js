fad3a4bc6a3e1e6036eeacecc0b2606e0aa405db
Ken Depelchin <ken@skedify.co>
2021-03-29 15:14:10 +0200

    feat: add schedulable filter to listings

--- src/resources/listings.js
+++ src/resources/listings.js
@@ -1,7 +1,7 @@
 import createResourceDescription from './util/createResourceDescription'
 
 export const listings = createResourceDescription('listings', {
-  filters: ['external_id', 'uuid'],
+  filters: ['external_id', 'schedulable', 'uuid'],
   enable_pagination: false,
   enable_id_filter: false,
 })
