bf183391850c46d380b26bf3c2a835ee5694e2e8
Ken Depelchin <ken@skedify.co>
2021-03-29 15:17:14 +0200

    feat: enable pagination on listings

--- src/resources/listings.js
+++ src/resources/listings.js
@@ -2,6 +2,5 @@ import createResourceDescription from './util/createResourceDescription'
 
 export const listings = createResourceDescription('listings', {
   filters: ['external_id', 'schedulable', 'uuid'],
-  enable_pagination: false,
   enable_id_filter: false,
 })
