e4f3de71955ca894f798b165c8d13b9c347db87a
Timo Suy <timothy.suy@gmail.com>
2021-04-28 15:57:57 +0200

    feat: add mark_range_as_busy_on_failure filter to unavailability

--- src/resources/unavailability.js
+++ src/resources/unavailability.js
@@ -11,6 +11,7 @@ export const unavailability = createResourceDescription('unavailability', {
     'subjects',
     { name: 'timezone', key: 'format[timezone]' },
     'ignore_skedify_appointments',
+    'mark_range_as_busy_on_failure',
   ],
   enable_pagination: false,
   enable_id_filter: false,
