792bec415ececeff4f6bac0fe4115aa4285a5120
Ken Depelchin <ken@skedify.co>
2021-05-04 11:30:05 +0200

    feat: add favorite contacts subresource to employees

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,18 +1,27 @@
 import createResourceDescription from './util/createResourceDescription'
-import { HTTP_VERB_POST } from '../constants'
+import { HTTP_VERB_DELETE, HTTP_VERB_GET, HTTP_VERB_POST } from '../constants'
 
-export const employees = createResourceDescription('employees', {
-  includes: ['user', 'enterprise', 'roles'],
-  filters: ['email', 'external_id', 'state', 'roles', 'match'],
-  headers: {
-    [HTTP_VERB_POST]: {
-      suppress_activation_email: (value) => {
-        if (value !== undefined) {
-          return { 'X-suppress-activation-email': true }
-        }
+export const employees = createResourceDescription(
+  'employees',
+  {
+    includes: ['user', 'enterprise', 'roles'],
+    filters: ['email', 'external_id', 'state', 'roles', 'match'],
+    headers: {
+      [HTTP_VERB_POST]: {
+        suppress_activation_email: (value) => {
+          if (value !== undefined) {
+            return { 'X-suppress-activation-email': true }
+          }
 
-        return undefined
+          return undefined
+        },
       },
     },
   },
-})
+  {
+    favoriteContacts: createResourceDescription('favorite_contacts', {
+      allowed_methods: [HTTP_VERB_GET, HTTP_VERB_POST, HTTP_VERB_DELETE],
+      filters: ['office_id', 'contact_id'],
+    }),
+  }
+)
