803d5baa92d3448bccb8f25928e52ba8953ba696
Thomas Simoens <thomas.simoens@skedify.co>
2021-05-25 13:21:41 +0000

    refactor(resources): rename emailConfigs to emailSettings
    BREAKING CHANGE: The resource emailConfigs will no longer be available, use emailSettings instead

--- src/resources/emailSettings.js
+++ src/resources/emailSettings.js
@@ -1,6 +1,6 @@
 import createResourceDescription from './util/createResourceDescription'
 
-export const emailConfigs = createResourceDescription('email_configs', {
+export const emailSettings = createResourceDescription('email_settings', {
   enable_pagination: false,
   enable_id_filter: false,
 })
