d53b7d5b26fe0bf941278fc899af1ff3be1889d4
Ken Depelchin <ken@skedify.co>
2021-08-17 16:39:18 +0200

    feat: add response.paging

---
+++ src/decorators/withResources/addPagingResponse.js
@@ -0,0 +1,42 @@
+function transformMeta(meta) {
+  const { total, per_page, current_page, last_page } = meta
+
+  const api = {
+    hasNext: () => current_page < last_page,
+    hasPrevious: () => current_page !== 1,
+  }
+
+  return {
+    size: per_page,
+    current: current_page,
+    total,
+    pages: last_page,
+    ...api,
+  }
+}
+
+function isPaging(meta) {
+  if (!meta) {
+    return false
+  }
+
+  return ['total', 'per_page', 'current_page', 'last_page'].every((key) =>
+    Object.keys(meta).includes(key)
+  )
+}
+
+export default function addPagingResponse(response) {
+  const { meta } = response
+
+  if (isPaging(meta)) {
+    Object.assign(
+      response,
+      {},
+      {
+        paging: { ...transformMeta(meta) },
+      }
+    )
+  }
+
+  return response
+}
